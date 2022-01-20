dc9dd9a0a323688d9d516a30e8353caac3582d03
Wen-Lin Lee <82gogolee@gmail.com>
2022-01-20 20:07:33 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector('.navbar');
+const fixNav = function () {
+  //   console.log(window.scrollY, nav.offsetHeight);
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add('active');
+  } else {
+    nav.classList.remove('active');
+  }
+};
+window.addEventListener('scroll', fixNav);
