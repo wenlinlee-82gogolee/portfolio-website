c4c3dcb9ae66f89904c00813d3bc8b25528ac07e
Wen-Lin Lee <82gogolee@gmail.com>
2022-01-27 12:35:27 +0800

    -add styles and make improvements on the layout section of the  project section -include sass for development

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-const nav = document.querySelector('.navbar');
-const fixNav = function () {
-  // console.log(window.scrollY, nav.offsetHeight);
-  if (window.scrollY > nav.offsetHeight + 150) {
-    nav.classList.add('active');
-  } else {
-    nav.classList.remove('active');
-  }
-};
-window.addEventListener('scroll', fixNav);
+const navIcon = document.querySelector('.navbar__icon');
+const navbar = document.querySelector('.navbar');
+navIcon.addEventListener('click', () => {
+  navbar.style.opacity = '100';
+});
