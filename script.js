ee8f9c969e061b0c5a188f5ec45d4d8c0ddf9a4f
Wen-Lin Lee <82gogolee@gmail.com>
2022-01-30 22:06:05 +0800

    -navbar move to the top of the page -sticky navbar -change the position of content a little bit for the about me section -create a gid layout for the skills sections -finish the layout for the projects, and contact me section. -change overall styling -rocket-like version

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-const navIcon = document.querySelector('.navbar__icon');
-const navbar = document.querySelector('.navbar');
-navIcon.addEventListener('click', () => {
-  navbar.style.opacity = '100';
-});
+const nav = document.querySelector('.navbar');
+const fixNav = function () {
+  //   console.log(window.scrollY, nav.offsetHeight);
+  if (window.scrollY > nav.offsetHeight) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+};
+window.addEventListener('scroll', fixNav);
+
+const aboutWenLin = {
+  name: ['李文菱', 'Wen-Lin Lee'],
+  email: '82gogolee@gmail.com',
+  nationality: 'Taiwan',
+  location: 'Taipei, Taiwan',
+  language: ['mandarin', 'english', 'taiwanese'],
+  education: 'Bachelor of Business Administration',
+  skills: [
+    'html',
+    'css',
+    'javaScript',
+    'react',
+    'jQuery',
+    'bootstrap',
+    'sass',
+    'npm',
+    'mySQL',
+    'linux',
+    'figma',
+    'git',
+    'github',
+  ],
+  desiredJobPosition: 'Front-end developer',
+  introduceMySelf: function () {
+    console.log(`I'M ${this.name[1].toUpperCase()}
+    `);
+  },
+};
+
+aboutWenLin.introduceMySelf();
